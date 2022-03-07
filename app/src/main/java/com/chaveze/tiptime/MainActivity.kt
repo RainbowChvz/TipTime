1a79f3013bc46b766823b26906bfb767897a01c7
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-03-07 11:40:40 -0800

    Enable view binding

--- app/src/main/java/com/chaveze/tiptime/MainActivity.kt
+++ app/src/main/java/com/chaveze/tiptime/MainActivity.kt
@@ -2,10 +2,14 @@ package com.chaveze.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chaveze.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
